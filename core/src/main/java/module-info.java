d1767475e769e69d16cc1040891ae1e998d73cd0
Peter Verhas <peter@verhas.com>
2018-08-14 20:28:27 +0200

    engine and core module

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module core {
+    exports javax0.jamal.builtins;
+    provides javax0.jamal.api.Macro with javax0.jamal.builtins.Define;
     requires javax0.jamal.api;
     requires  javax0.jamal.tools;
-    uses javax0.jamal.api.Macro;
 }
---
+++ engine/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module engine {
+    requires javax0.jamal.api;
+    requires  javax0.jamal.tools;
+    uses javax0.jamal.api.Macro;
+}
