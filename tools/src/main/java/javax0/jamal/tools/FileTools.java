8e05db8922d6afee08855fba5a0e066b4667e382
Peter_Verhas <peter@verhas.com>
2018-08-31 16:49:18 +0200

    removed home made implementation that was already implemented in the Paths class since Java 7

--- tools/src/main/java/javax0/jamal/tools/FileTools.java
+++ tools/src/main/java/javax0/jamal/tools/FileTools.java
@@ -29,9 +29,6 @@ public class FileTools {
                 && fileName.charAt(1) == ':')) {
             return fileName;
         }
-        if (fileName.startsWith("./")) {
-            fileName = fileName.substring(2);
-        }
         var z = reference.lastIndexOf("/");
         if (z > -1) {
             reference = reference.substring(0, z + 1);
@@ -39,23 +36,10 @@ public class FileTools {
             reference = "";
         }
 
-        var absoluteFileName = (reference + fileName).replaceAll("//", "/");
-        var path = new ArrayList<>(Arrays.asList(absoluteFileName.split("/")));
-        var i = 0;
-        while (i < path.size() - 1) {
-            if (".".equals(path.get(i))) {
-                path.remove(i);
-            } else if (!"..".equals(path.get(i)) && "..".equals(path.get(i + 1))) {
-                path.remove(i + 1);
-                path.remove(i);
-                if (i > 0) {
-                    i--;
-                }
-            } else {
-                i++;
-            }
-        }
-        return String.join("/", path);
+        return Paths.get(reference)
+                .resolve(Paths.get(fileName))
+                .normalize()
+                .toString()
+                .replaceAll("\\\\","/");
     }
-
 }
