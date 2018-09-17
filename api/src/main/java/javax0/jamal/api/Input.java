e58d75c4db8c65e2f4a98c2ea2392b25b451dcb2
Peter_Verhas <peter@verhas.com>
2018-09-17 14:27:07 +0200

    input class documentation

--- api/src/main/java/javax0/jamal/api/Input.java
+++ api/src/main/java/javax0/jamal/api/Input.java
@@ -1,5 +1,10 @@
 package javax0.jamal.api;
 
+/**
+ * An input has a string builder and also a file name from where the input is coming from. The
+ * string builder is used to fetch the characters. The reference file name is used to construct the
+ * name of the files in case a macro like {@code import} or {@code include} needs to open another file.
+ */
 public interface Input {
     StringBuilder getInput();
 
