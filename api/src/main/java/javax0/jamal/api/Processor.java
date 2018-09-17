d7261ab3c4dae375a9ee02f8598d44996f823aff
Peter_Verhas <peter@verhas.com>
2018-09-17 14:28:07 +0200

    interface Processor extended with comlimentary default methods

--- api/src/main/java/javax0/jamal/api/Processor.java
+++ api/src/main/java/javax0/jamal/api/Processor.java
@@ -2,6 +2,24 @@ package javax0.jamal.api;
 
 public interface Processor {
     String process(final Input in) throws BadSyntax;
+
     MacroRegister getRegister();
-    UserDefinedMacro newUserDefinedMacro(String id, String input, String[]  params) throws BadSyntax;
+
+    UserDefinedMacro newUserDefinedMacro(String id, String input, String[] params) throws BadSyntax;
+
+    default boolean isDefined(String id) {
+        return getRegister().getUserMacro(id).isPresent();
+    }
+
+    default void defineGlobal(UserDefinedMacro macro) {
+        getRegister().global(macro);
+    }
+
+    default void define(UserDefinedMacro macro){
+        getRegister().define(macro);
+    }
+
+    default void separators(String openMacro, String closeMacro) throws BadSyntax{
+        getRegister().separators(openMacro, closeMacro);
+    }
 }
