7ef8bf99b633bab558b901b3a41d0d270488fd78
Peter_Verhas <peter@verhas.com>
2018-09-17 14:27:32 +0200

    MacroRegister.java was reformatted

--- api/src/main/java/javax0/jamal/api/MacroRegister.java
+++ api/src/main/java/javax0/jamal/api/MacroRegister.java
@@ -2,14 +2,22 @@ package javax0.jamal.api;
 
 import java.util.Optional;
 
-public interface MacroRegister extends Delimiters{
+public interface MacroRegister extends Delimiters {
     Optional<Macro> geMacro(String id);
+
     Optional<UserDefinedMacro> getUserMacro(String id);
+
     void global(UserDefinedMacro macro);
+
     void global(Macro macro);
+
     void define(UserDefinedMacro macro);
+
     void define(Macro macro);
+
     void export(String id) throws BadSyntax;
+
     void push();
+
     void pop();
 }
