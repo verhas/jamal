f8c4e14077322cfb4a05d98ee1181bb69b4ea6cd
Peter_Verhas <peter@verhas.com>
2019-02-26 16:43:30 +0100

    split optimized not to be recursive

--- jamal-engine/src/main/java/javax0/jamal/engine/macro/ParameterSegment.java
+++ jamal-engine/src/main/java/javax0/jamal/engine/macro/ParameterSegment.java
@@ -1,6 +1,11 @@
 package javax0.jamal.engine.macro;
 
 public class ParameterSegment extends Segment {
+    public ParameterSegment(Segment nextSeg, String text) {
+        this.nextSeg = nextSeg;
+        this.text = text;
+    }
+
     public void split(String parameter) {
         // NOT SPLITTABLE
     }
--- jamal-engine/src/main/java/javax0/jamal/engine/macro/TextSegment.java
+++ jamal-engine/src/main/java/javax0/jamal/engine/macro/TextSegment.java
@@ -1,9 +1,39 @@
 package javax0.jamal.engine.macro;
 
 public class TextSegment extends Segment {
-    public TextSegment(Segment next, String content){
+    public TextSegment(Segment next, String content) {
         this.nextSeg = next;
         this.text = content;
+    }
+
+    /**
+     * Iterate over the segments and perform the splitting.
+     *
+     * @param root      the segment to split
+     * @param parameter the name of the parameter that is used to spit the segment
+     */
+    private static void split(final TextSegment root, final String parameter) {
+        var it = root;
+        while ((it = splitAndGetNext(it, parameter)) != null) ;
+    }
+
+    /**
+     * Split up the segment to three segment: a text segment with text before the parameter text, a parameter segment
+     * and a text segment with the text after the parameter and after this is done return the last text segment.
+     *
+     * @param it the text segment to split
+     * @param parameter the parameter that splits the segment into two
+     * @return the second text segment or null in case the segment can not be split.
+     */
+    private static TextSegment splitAndGetNext(final TextSegment it, final String parameter) {
+        final var start = it.text.indexOf(parameter);
+        if (start < 0) {
+            return null;
+        }
+        final var textSeg = new TextSegment(it.nextSeg, it.text.substring(start + parameter.length()));
+        it.nextSeg = new ParameterSegment(textSeg, parameter);
+        it.text = it.text.substring(0, start);
+        return textSeg;
     }
 
     /**
@@ -17,14 +47,14 @@ public class TextSegment extends Segment {
      * <li> The second segment is a parameter segment, that will contain the name of the parameter
      * <li> The last segment will contain the text of the original text segment that is after the parameter.
      * </ol>
-     *
+     * <p>
      * The catch is that before returning the last text segment is also split for the same parameter. That way the
      * segment originally being
      *
      * <pre>
      *     bla bla ... P ... bla bla .... P ... bla bla... P ... bla
      * </pre>
-     *
+     * <p>
      * will become
      *
      * <pre>
@@ -36,7 +66,7 @@ public class TextSegment extends Segment {
      *     PARAM P
      *     TEXT ... bla
      * </pre>
-     *
+     * <p>
      * seven segments. The segments will be chained to each other forward through the {@code nextSeg} field that is
      * returned by the {@link #next()} method. The whole splitting mechanism modifies this chain while the same time
      * iterating through it. Since the parameter segments {@link ParameterSegment} are not splittable any more this
@@ -47,15 +77,7 @@ public class TextSegment extends Segment {
      */
     @Override
     public void split(String parameter) {
-        var start = text.indexOf(parameter);
-        if (start != -1) {
-            final var textSeg = new TextSegment(nextSeg, text.substring(start + parameter.length()));
-            textSeg.split(parameter);
-            final var parSeg = new ParameterSegment();
-            parSeg.text = parameter;
-            parSeg.nextSeg = textSeg;
-            text = text.substring(0, start);
-            nextSeg = parSeg;
-        }
+        split(this, parameter);
     }
+
 }
