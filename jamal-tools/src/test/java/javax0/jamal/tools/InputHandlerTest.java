323426d5db4490a09d216389c4e2d6c00041845c
Peter Verhas <peter@verhas.com>
2019-03-21 11:50:58 +0100

    test updated

--- jamal-tools/src/test/java/javax0/jamal/tools/InputHandlerTest.java
+++ jamal-tools/src/test/java/javax0/jamal/tools/InputHandlerTest.java
@@ -1,6 +1,7 @@
 package javax0.jamal.tools;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class InputHandlerTest {
@@ -13,14 +14,50 @@ public class InputHandlerTest {
     }
 
     @Test
-    void splitsInputCorrectly() {
-        assertSplit("/a/b/c/d/e/f/g/h/", "a", "b", "c", "d", "e", "f", "g", "h", "");
-        assertSplit("/a/b/c//e/f/g/h/", "a", "b", "c", "", "e", "f", "g", "h", "");
-        assertSplit("  /a/b/c//e/f/g/h/", "a", "b", "c", "", "e", "f", "g", "h", "");
-        assertSplit("  /a/b/c//e/f/g/h", "a", "b", "c", "", "e", "f", "g", "h");
+    @DisplayName("input starts with separator, split correctly")
+    void splitsInputCorrectlyWhenStartingWithSeparator() {
+        assertSplit("/a/b/c/d/e/f/g/h", "a", "b", "c", "d", "e", "f", "g", "h");
+    }
 
-        assertSplit("`\\s*` abcdefgh", "", "", "a", "b", "c", "d", "e", "f", "g", "h", "");
+    @Test
+    @DisplayName("first character treated as separator with leading whitespaces ignored")
+    void splitsInputCorrectlyIgnoringStartingWhitespaces() {
+        assertSplit("  /a/b/c//e/f/g/h", "a", "b", "c", "", "e", "f", "g", "h");
+    }
+
+    @Test
+    @DisplayName("consecutive separators treated as distinct separators")
+    void splitsInputCorrectlyWhenStartingWithSeparatorWithConsecutiveSeparators() {
+        assertSplit("/a/b/c//e/f/g/h", "a", "b", "c", "", "e", "f", "g", "h");
+    }
+
+    @Test
+    @DisplayName("if input ends with separator, the last item is empty")
+    void splitsInputCorrectlyWhenEndingWithEmptyInstance() {
+        assertSplit("  /a/b/c//e/f/g/h/", "a", "b", "c", "", "e", "f", "g", "h", "");
+    }
+
+    @Test
+    @DisplayName("input starts with regular expression")
+    void splitsInputCorrectlyStartingWithRegularExpression() {
+        assertSplit("`[0-9]`a3b4c6d7e8f9g0h", "a", "b", "c", "d", "e", "f", "g", "h");
+    }
+
+    @Test
+    @DisplayName("separator is the backtick character")
+    void splitsInputCorrectlyWithBacktickAsSeparator() {
         assertSplit("```` a`b`c`d`e`f`g`h", " a", "b", "c", "d", "e", "f", "g", "h");
+    }
+
+    @Test
+    @DisplayName("separator is regular expression containing special (backslash-escaped) character")
+    void splitsInputCorrectlyWithRegularExpressionContainingSpecialCharacter() {
+        assertSplit("`\\s*` abcdefgh", "", "", "a", "b", "c", "d", "e", "f", "g", "h", "");
+    }
+
+    @Test
+    @DisplayName("separator includes backtick AND regular expression")
+    void splitsInputCorrectlyWhenStartingWithComplexRegularExpression() {
         assertSplit("```\\w{2}` a`hiba`wucontal`d0`e`f`g`h", " a", "ba", "contal","`e`f`g`h");
     }
 }
