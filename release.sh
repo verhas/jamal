40383e7dc303f5d8cd2fc036eac2d0deb08e8b00
Peter Verhas <peter@verhas.com>
2021-01-24 19:37:18 +0100

    release 1.6.0

--- release.sh
+++ release.sh
@@ -13,6 +13,7 @@ echo [INFO]   mvn verify
 echo [INFO]
 echo [INFO] if all compilation went well, then and only then:
 echo [INFO]
+echo [INFO]   git commit -m "new release <VERSION>"
 echo [INFO]   mvn deploy -Possrh
 echo [INFO] -------------------------------------------------------
 echo [INFO]
