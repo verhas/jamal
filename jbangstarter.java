1d2dd2d735dae1d39b3221d1301819fb138188f2
Peter Verhas <peter@verhas.com>
2021-05-12 15:54:32 +0200

    release 1.7.6

--- jbangstarter.java
+++ jbangstarter.java
@@ -1,20 +1,20 @@
 ///usr/bin/env jbang \"$0\" \"$@\" ; exit $?
 //JAVA 11+
 //DEPS info.picocli:picocli:4.6.1
-//DEPS com.javax0.jamal:jamal-engine:1.7.5
-//DEPS com.javax0.jamal:jamal-api:1.7.5
-//DEPS com.javax0.jamal:jamal-tools:1.7.5
-//DEPS com.javax0.jamal:jamal-core:1.7.5
-//DEPS com.javax0.jamal:jamal-cmd:1.7.5
-//DEPS com.javax0.jamal:jamal-snippet:1.7.5
-//DEPS com.javax0.jamal:jamal-scriptbasic:1.7.5
-//DEPS com.javax0.jamal:jamal-groovy:1.7.5
-//DEPS com.javax0.jamal:jamal-ruby:1.7.5
-//DEPS com.javax0.jamal:jamal-plantuml:1.7.5
-//DEPS com.javax0.jamal:jamal-debug:1.7.5
-//DEPS com.javax0.jamal:jamal-jamal:1.7.5
-//DEPS com.javax0.jamal:jamal-yaml:1.7.5
-//DEPS com.javax0.jamal:jamal-io:1.7.5
+//DEPS com.javax0.jamal:jamal-engine:1.7.6
+//DEPS com.javax0.jamal:jamal-api:1.7.6
+//DEPS com.javax0.jamal:jamal-tools:1.7.6
+//DEPS com.javax0.jamal:jamal-core:1.7.6
+//DEPS com.javax0.jamal:jamal-cmd:1.7.6
+//DEPS com.javax0.jamal:jamal-snippet:1.7.6
+//DEPS com.javax0.jamal:jamal-scriptbasic:1.7.6
+//DEPS com.javax0.jamal:jamal-groovy:1.7.6
+//DEPS com.javax0.jamal:jamal-ruby:1.7.6
+//DEPS com.javax0.jamal:jamal-plantuml:1.7.6
+//DEPS com.javax0.jamal:jamal-debug:1.7.6
+//DEPS com.javax0.jamal:jamal-jamal:1.7.6
+//DEPS com.javax0.jamal:jamal-yaml:1.7.6
+//DEPS com.javax0.jamal:jamal-io:1.7.6
 
 
 
