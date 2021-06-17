67220bc23c755cb5ce89e5679acfdae1184975f2
Peter Verhas <peter@verhas.com>
2021-06-17 23:45:54 +0200

    tests sepatrated to locate the windows CI error

--- jamal-test/src/test/java/javax0/jamal/test/core/TestCore.java
+++ jamal-test/src/test/java/javax0/jamal/test/core/TestCore.java
@@ -7,31 +7,128 @@ import org.junit.jupiter.api.TestFactory;
 public class TestCore {
 
     @TestFactory
-    JamalTests<?> testAllCoreMacros() {
+    JamalTests<?> testAllCoreMacros1() {
         return JamalYamlTest.factory(
-            "TestFor",
-            //JamalYamlTest.__OFF__,
-            "TestSamples",
-            "TestDefine",
-            "TestBeginEnd",
-            "TestBlock",
-            "TestComment",
-            "TestEscape",
-            "TestEval",
-            "TestExport",
-            "TestIdent",
-            "TestIf",
-            "TestImport",
-            "TestJShell",
-            "TestNewLineOptions",
-            "TestOptions",
-            "TestRecursiveMacro",
-            "TestRequire",
-            "TestTry",
-            "TestUndefine",
-            "TestUse",
-            "TestSep",
-            "TestEngine",
+            "TestFor");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacros2() {
+        return JamalYamlTest.factory(
+            "TestSamples");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacros3() {
+        return JamalYamlTest.factory(
+            "TestDefine");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacros4() {
+        return JamalYamlTest.factory(
+            "TestBlock");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacros5() {
+        return JamalYamlTest.factory(
+            "TestComment");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacros6() {
+        return JamalYamlTest.factory(
+            "TestEscape");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacros7() {
+        return JamalYamlTest.factory(
+            "TestEval");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacros8() {
+        return JamalYamlTest.factory(
+            "TestExport");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacros9() {
+        return JamalYamlTest.factory(
+            "TestIdent");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacrosA() {
+        return JamalYamlTest.factory(
+            "TestIf");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacrosB() {
+        return JamalYamlTest.factory(
+            "TestJShell");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacrosC() {
+        return JamalYamlTest.factory(
+            "TestNewLineOpions");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacrosD() {
+        return JamalYamlTest.factory(
+            "TestOptions");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacrosE() {
+        return JamalYamlTest.factory(
+            "TestRecursiveacro");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacrosF() {
+        return JamalYamlTest.factory(
+            "TestRequire");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacrosG() {
+        return JamalYamlTest.factory(
+            "TestTry");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacrosH() {
+        return JamalYamlTest.factory(
+            "TestUndefine");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacrosI() {
+        return JamalYamlTest.factory(
+            "TestUse");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacrosJ() {
+        return JamalYamlTest.factory(
+            "TestSep");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacrosK() {
+        return JamalYamlTest.factory(
+            "TestEngine");
+    }
+
+    @TestFactory
+    JamalTests<?> testAllCoreMacrosL() {
+        return JamalYamlTest.factory(
             "nested_ud_macros"
         );
     }
