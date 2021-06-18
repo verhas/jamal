488a71648caaca384c27a0d8631d002672907bc4
Peter Verhas <peter@verhas.com>
2021-06-18 10:56:15 +0200

    TestWrite checks that it really writes to the standard error.

--- jamal-io/src/test/java/javax0/jamal/io/TestWrite.java
+++ jamal-io/src/test/java/javax0/jamal/io/TestWrite.java
@@ -1,24 +1,37 @@
 package javax0.jamal.io;
 
 import javax0.jamal.testsupport.TestThat;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 public class TestWrite {
 
     @Test
     void testSimpleWrite() throws Exception {
+        final var saveErr = System.err;
+        final var out = new ByteArrayOutputStream();
+        final var newErr = new PrintStream(out);
+        System.setErr(newErr);
         TestThat.theInput("" +
-            "{@define fileName=./target/wups/file.txt}" +
-            "{#io:write (file={fileName} mkdir)" +
-            "this is the text into the\n" +
-            "file\n" +
-            "}{#include [verbatim] {fileName}}" +
-            "{#io:remove recursive file=./target/wups}" +
-            "{@io:print (err)I'm done}")
+                "{@define fileName=./target/wups/file.txt}" +
+                "{#io:write (file={fileName} mkdir)" +
+                "this is the text into the\n" +
+                "file\n" +
+                "}{#include [verbatim] {fileName}}" +
+                "{#io:remove recursive file=./target/wups}" +
+                "{@io:print (err)I'm done}")
             .ignoreLineEnding()
             .results("" +
                 "this is the text into the\n" +
                 "file"
             );
+        newErr.close();
+        out.close();
+        Assertions.assertEquals("I'm done", out.toString(StandardCharsets.UTF_8));
+        System.setErr(saveErr);
     }
 }
