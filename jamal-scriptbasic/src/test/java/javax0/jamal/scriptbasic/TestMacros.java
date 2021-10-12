a8e5517ae64e7465eab81e8227a564f502f5d7b3
Peter Verhas <peter@verhas.com>
2021-10-12 17:42:29 +0200

    ScriptBasic test and readme was fixed, the sample made no sense at all

--- jamal-scriptbasic/src/test/java/javax0/jamal/scriptbasic/TestMacros.java
+++ jamal-scriptbasic/src/test/java/javax0/jamal/scriptbasic/TestMacros.java
@@ -11,35 +11,36 @@ public class TestMacros {
         TestThat.theInput(
             // snippet sample
             "{@import res:scriptbasic.jim}\n" +
-            "{expr 13+1}{@define start=1}{@define end=13}\n" +
-            "{#basic for i={start} to {end}\n" +
-            "if i%2 = 1 then\n" +
-            "  oddity = \"odd\"\n" +
-            "else\n" +
-            "  oddity = \"even\"\n" +
-            "endif\n" +
-            "print i,\". is an \",oddity,\" number\\n\"\n" +
-            "next\n" +
-            "}"
+                "{expr 13+1}{@define start=1}{@define end=13}\n" +
+                "{#basic for i={start} to {end}\n" +
+                "if i%2 = 1 then\n" +
+                "  oddity = \"odd\"\n" +
+                "else\n" +
+                "  oddity = \"even\"\n" +
+                "endif\n" +
+                "print i,\". is an \",oddity,\" number\\n\"\n" +
+                "next\n" +
+                "}\n"
             // end snippet
         ).results(
             // snippet sample_output
             "\n" +
-            "14\n" +
-            "1. is an odd number\n" +
-            "2. is an even number\n" +
-            "3. is an odd number\n" +
-            "4. is an even number\n" +
-            "5. is an odd number\n" +
-            "6. is an even number\n" +
-            "7. is an odd number\n" +
-            "8. is an even number\n" +
-            "9. is an odd number\n" +
-            "10. is an even number\n" +
-            "11. is an odd number\n" +
-            "12. is an even number\n" +
-            "13. is an odd number\n");
-            // end snippet
+                "14\n" +
+                "1. is an odd number\n" +
+                "2. is an even number\n" +
+                "3. is an odd number\n" +
+                "4. is an even number\n" +
+                "5. is an odd number\n" +
+                "6. is an even number\n" +
+                "7. is an odd number\n" +
+                "8. is an even number\n" +
+                "9. is an odd number\n" +
+                "10. is an even number\n" +
+                "11. is an odd number\n" +
+                "12. is an even number\n" +
+                "13. is an odd number\n" +
+                "\n");
+        // end snippet
     }
 
 }
