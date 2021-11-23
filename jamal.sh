e69449fa31a1a3d03e12dd0f5539bb5ab5684fd4
Peter Verhas <peter@verhas.com>
2021-11-23 19:22:53 +0100

    1.10.0 release
    Signed-off-by: Peter Verhas <peter@verhas.com>

--- jamal.sh
+++ jamal.sh
@@ -2,7 +2,7 @@
 MODULES="api engine core tools cmd"
 REPO=$HOME/.m2/repository
 REPO_JAMAL=$REPO/com/javax0/jamal
-VERSION=1.10.0-SNAPSHOT
+VERSION=1.10.0
 CENTRAL=https://repo1.maven.org/maven2
 
 for MODULE in $MODULES ; do
