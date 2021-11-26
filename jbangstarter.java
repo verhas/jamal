ece1e54e79ad2b8c811833c88d0ff27dd8346645
Peter Verhas <peter@verhas.com>
2021-11-26 10:35:11 +0100

    release 1.10.1
    Signed-off-by: Peter Verhas <peter@verhas.com>

--- jbangstarter.java
+++ jbangstarter.java
@@ -1,21 +1,21 @@
 ///usr/bin/env jbang \"$0\" \"$@\" ; exit $?
 //JAVA 11+
 //DEPS info.picocli:picocli:4.6.1
-//DEPS com.javax0.jamal:jamal-engine:1.10.0
-//DEPS com.javax0.jamal:jamal-api:1.10.0
-//DEPS com.javax0.jamal:jamal-tools:1.10.0
-//DEPS com.javax0.jamal:jamal-core:1.10.0
-//DEPS com.javax0.jamal:jamal-cmd:1.10.0
-//DEPS com.javax0.jamal:jamal-snippet:1.10.0
-//DEPS com.javax0.jamal:jamal-scriptbasic:1.10.0
-//DEPS com.javax0.jamal:jamal-groovy:1.10.0
-//DEPS com.javax0.jamal:jamal-ruby:1.10.0
-//DEPS com.javax0.jamal:jamal-plantuml:1.10.0
-//DEPS com.javax0.jamal:jamal-debug:1.10.0
-//DEPS com.javax0.jamal:jamal-jamal:1.10.0
-//DEPS com.javax0.jamal:jamal-yaml:1.10.0
-//DEPS com.javax0.jamal:jamal-io:1.10.0
-//DEPS com.javax0.jamal:jamal-assertions:1.10.0
+//DEPS com.javax0.jamal:jamal-engine:1.10.1
+//DEPS com.javax0.jamal:jamal-api:1.10.1
+//DEPS com.javax0.jamal:jamal-tools:1.10.1
+//DEPS com.javax0.jamal:jamal-core:1.10.1
+//DEPS com.javax0.jamal:jamal-cmd:1.10.1
+//DEPS com.javax0.jamal:jamal-snippet:1.10.1
+//DEPS com.javax0.jamal:jamal-scriptbasic:1.10.1
+//DEPS com.javax0.jamal:jamal-groovy:1.10.1
+//DEPS com.javax0.jamal:jamal-ruby:1.10.1
+//DEPS com.javax0.jamal:jamal-plantuml:1.10.1
+//DEPS com.javax0.jamal:jamal-debug:1.10.1
+//DEPS com.javax0.jamal:jamal-jamal:1.10.1
+//DEPS com.javax0.jamal:jamal-yaml:1.10.1
+//DEPS com.javax0.jamal:jamal-io:1.10.1
+//DEPS com.javax0.jamal:jamal-assertions:1.10.1
 
 
 
