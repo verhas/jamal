4a7e78df2cbcdfcfb47670320e6d4ae82de9f703
Peter Verhas <peter@verhas.com>
2022-01-14 09:22:26 +0100

    DocumentConverter was extended and LICENSE file was added, because JetBrains OSS automated check sucks
    Signed-off-by: Peter Verhas <peter@verhas.com>

--- jamal-engine/src/main/java/javax0/jamal/DocumentConverter.java
+++ jamal-engine/src/main/java/javax0/jamal/DocumentConverter.java
@@ -1,7 +1,26 @@
 package javax0.jamal;
 
+import javax0.jamal.api.Position;
 import javax0.jamal.engine.Processor;
 import javax0.jamal.tools.FileTools;
+import javax0.jamal.tools.Input;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.StandardOpenOption.WRITE;
 
 /**
  * Convert one single document.
@@ -40,4 +59,105 @@ public class DocumentConverter {
         final var output = file.substring(0, file.length() - ".jam".length());
         FileTools.writeFileContent(output, result);
     }
+
+    private static class Includes {
+        private final String[] includes;
+
+        private Includes(String[] includes) {
+            this.includes = includes;
+        }
+
+        Stream<String> stream(){
+            return Arrays.stream(includes);
+        }
+    }
+
+    private static class Excludes {
+        private final String[] excludes;
+
+        private Excludes(String[] excludes) {
+            this.excludes = excludes;
+        }
+        Stream<String> stream(){
+            return Arrays.stream(excludes);
+        }
+    }
+
+    public static Includes include(String... s) {
+        return new Includes(s);
+    }
+
+    public static Excludes exclude(String... s) {
+        return new Excludes(s);
+    }
+
+    public static void convertAll(Includes include, Excludes exclude) throws Exception {
+        for (final var p :
+            Files.walk(Paths.get(getRoot()), Integer.MAX_VALUE)
+                .filter(Files::isRegularFile)
+                .filter(s -> include.stream().anyMatch(z -> s.toString().endsWith(z)) && exclude.stream().noneMatch(z -> s.toString().endsWith(z)))
+                .collect(Collectors.toList())) {
+            executeJamal(p, Paths.get(p.toString().replaceAll("\\.jam$", "")));
+        }
+    }
+
+    private static void executeJamal(final Path inputPath, final Path outputPath) throws Exception {
+        final String result;
+        try (final var processor = new Processor("{%", "%}")) {
+            result = processor.process(createInput(inputPath));
+        }
+        Files.writeString(outputPath, result, StandardCharsets.UTF_8, WRITE, TRUNCATE_EXISTING, CREATE);
+    }
+
+    private static Input createInput(Path inputFile) throws IOException {
+        var fileContent = Files.lines(inputFile).collect(Collectors.joining("\n"));
+        return new javax0.jamal.tools.Input(fileContent, new Position(inputFile.toString(), 1));
+    }
+
+    private static Optional<File> found(final StringBuilder sb) {
+        File rootDirFile = new File(sb.toString());
+        if (rootDirFile.exists()) {
+            return Optional.of(rootDirFile);
+        }
+        sb.insert(0, "../");
+        return Optional.empty();
+    }
+
+    /**
+     * Get the root directory of the project.
+     * <p>
+     * The code is starting from the current working directory and going upward tries to find a directory that contains
+     * a {@code .git}, {@code .mvn} or {@code ROOT.dir} file.
+     * The directories {@code .git} and {@code .mvn} appear only in the root directory of a project and not in module
+     * directories.
+     * If the project is not in a git repository or in a project that does not have an {@code .mvn} directory then a
+     * {@code ROOT.dir} placeholder has to be created in the root directory.
+     *
+     * @return
+     * @throws IOException
+     */
+    public static String getRoot() throws IOException {
+        final var dirs = Stream.of("ROOT.dir", ".git", ".mvn")
+            .map(StringBuilder::new)
+            .collect(Collectors.toList());
+
+        final Optional<File> file =
+            IntStream.range(0, 100).mapToObj(i ->
+                    dirs.stream()
+                        .map(DocumentConverter::found)
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .findFirst())
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst();
+
+        if (file.isPresent()) {
+            final var parent = file.get().getParentFile();
+            return parent == null ? "." : parent.getCanonicalPath();
+        } else {
+            throw new RuntimeException("Cannot find the root directory of the project file.");
+        }
+    }
+
 }
