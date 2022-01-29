32c8146a6981c5818a4fea8176f6622626721580
Peter Verhas <peter@verhas.com>
2022-01-29 20:45:44 +0100

    Identified.java comment was still referring to `options as an example deleted
    Signed-off-by: Peter Verhas <peter@verhas.com>

--- jamal-api/src/main/java/javax0/jamal/api/Identified.java
+++ jamal-api/src/main/java/javax0/jamal/api/Identified.java
@@ -11,12 +11,7 @@ package javax0.jamal.api;
  * </ul>
  * <p>
  * Some macros also want to implement something similar things that may or may not be accessed from the processed
- * jamal text, but are available and managed by the code. A good example is how the macro
- * {@code javax0.jamal.builtins.Options} is implemented. When the options is first defined it creates an instance of
- * {@code javax0.jamal.tools.OptionsStore}, which is neither a script, nor a macro and it cannot be evaluated, but
- * it has an identifier, which is {@code `options}. This object is stored along with the scripts and user defined
- * macros with the same life cycle as those. If an option was set in a local environment it will no affect environments
- * above unless it is exported.
+ * jamal text, but are available and managed by the code.
  */
 @FunctionalInterface
 public interface Identified {
