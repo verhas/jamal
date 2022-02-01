7abe5f2f986e1a63ee9a2caf25b9cec5c45712cc
Peter Verhas <peter@verhas.com>
2022-02-01 02:02:10 +0100

    include supports `lines` option
    Signed-off-by: Peter Verhas <peter@verhas.com>

--- jamal-core/src/main/java/javax0/jamal/builtins/Include.java
+++ jamal-core/src/main/java/javax0/jamal/builtins/Include.java
@@ -4,11 +4,13 @@ import javax0.jamal.api.BadSyntax;
 import javax0.jamal.api.EnvironmentVariables;
 import javax0.jamal.api.Input;
 import javax0.jamal.api.Macro;
+import javax0.jamal.api.Position;
 import javax0.jamal.api.Processor;
 import javax0.jamal.tools.Marker;
 import javax0.jamal.tools.Params;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 import static javax0.jamal.tools.FileTools.absolute;
 import static javax0.jamal.tools.FileTools.getInput;
@@ -36,14 +38,13 @@ public class Include implements Macro {
     @Override
     public String evaluate(Input input, Processor processor) throws BadSyntax {
         var position = input.getPosition();
-        final var top = Params.<Boolean>holder(null,"top").asBoolean();
-        final var verbatim = Params.<Boolean>holder("includeVerbatim","verbatim").asBoolean();
-        Params.using(processor).from(this).between("[]").keys(verbatim,top).parse(input);
-        if( top.is()){
-            while( position.parent != null ){
-                position = position.parent;
-            }
-        }
+        final var top = Params.<Boolean>holder(null, "top").asBoolean();
+        final var verbatim = Params.<Boolean>holder("includeVerbatim", "verbatim").asBoolean();
+        final var lines = Params.<Boolean>holder(null, "lines").asString();
+        Params.using(processor).from(this).between("[]").keys(verbatim, top, lines).parse(input);
+
+        position = repositionToTop(position, top);
+
         skipWhiteSpaces(input);
         var reference = position.file;
         var fileName = absolute(reference, input.toString().trim());
@@ -54,13 +55,104 @@ public class Include implements Macro {
         var marker = new Marker("{@include " + fileName + "}", position);
         final String result;
         processor.getRegister().push(marker);
-        if( verbatim.get() ){
-            result = getInput(fileName,position).toString();
-        }else {
-            result = processor.process(getInput(fileName,position));
+        final var includedInput = getInput(fileName, position);
+        if (lines.isPresent()) {
+            filterLines(includedInput, lines.get());
+        }
+        if (verbatim.get()) {
+            result = includedInput.toString();
+        } else {
+            result = processor.process(includedInput);
         }
         processor.getRegister().pop(marker);
         depth++;
         return result;
+    }
+
+    private static class Range {
+        final int from;
+        final int to;
+
+        private Range(final int from, final int to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        boolean in(int x) {
+            return x >= from && x <= to;
+        }
+    }
+
+    private static Range range(final int from, final int to) {
+        return new Range(from, to);
+    }
+
+    private void filterLines(final Input in, final String s) throws BadSyntax {
+        final var sb = in.getSB();
+        final var lines = sb.toString().split("\n", -1);
+        for (int i = 0; i < lines.length - 1; i++) {
+            lines[i] = lines[i] + "\n";
+        }
+        final var ranges = calculateRanges(s, lines.length);
+        sb.setLength(0);
+        for (final var range : ranges) {
+            final int step = range.to >= range.from ? 1 : -1;
+            for (int i = range.from; i != range.to + step; i += step) {
+                if (i <= lines.length && i > 0) {
+                    sb.append(lines[i - 1]);
+                }
+            }
+        }
+    }
+
+    private List<Range> calculateRanges(final String s, final int n) throws BadSyntax {
+        final var lines = s.split("[,;]");
+        final var ranges = new ArrayList<Range>();
+        for (final var line : lines) {
+            var range = line.split("\\.\\.");
+            if (range.length == 1) {
+                range = new String[]{line, line};
+            }
+            if (range.length != 2) {
+                throw new BadSyntax("The line range " + line + " is not valid");
+            }
+            int to, from;
+            try {
+                from = Integer.parseInt(range[0].trim());
+                to = Integer.parseInt(range[1].trim());
+            } catch (NumberFormatException nfe) {
+                throw new BadSyntax("The line range " + line + " is not valid");
+            }
+            if (from < 0) {
+                from += n;
+            }
+            if (to < 0) {
+                to += n;
+            }
+            if (from == 0 || to == 0) {
+                throw new BadSyntax("The line range " + line + " is not valid");
+            }
+            ranges.add(range(from, to));
+        }
+        return ranges;
+    }
+
+    /**
+     * Get the position of the 'include' from the input. When the 'top' parameter is true the position is the position
+     * of the root document even if the include macro is used several level deeper in included documents.
+     *
+     * @param input from which we will gauge the position
+     * @param top   flag to signal if we need to include from the top
+     * @return the effective position used to calculate the file location of the included file when specified as a
+     * relative file name
+     * @throws BadSyntax if 'top' is erroneous and querying it throws exception
+     */
+    private Position repositionToTop(Position position, final Params.Param<Boolean> top) throws BadSyntax {
+        if (top.is()) {
+            while (position.parent != null) {
+                position = position.parent;
+            }
+        }
+        return position;
     }
 }
---
+++ jamal-test/src/test/java/javax0/jamal/test/core/TestInclude.java
@@ -0,0 +1,69 @@
+package javax0.jamal.test.core;
+
+import javax0.jamal.testsupport.TestThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestInclude {
+
+    @Test
+    @DisplayName("You can include only certain lines from a file")
+    void testPartialInclude() throws Exception {
+        TestThat.theInput("{@include [lines=1..2] res:import/lineNumbers.txt}").results("01\n02\n");
+    }
+
+    @Test
+    @DisplayName("Multiple ranges can be specified")
+    void testMultipleRanges() throws Exception {
+        TestThat.theInput("{@include [lines=1..2,10..14,16..18] res:import/lineNumbers.txt}").results("01\n02\n10\n11\n12\n13\n14\n16\n17\n18\n");
+    }
+
+    @Test
+    @DisplayName("You can specify a range using negative numbers")
+    void testNegativeRange() throws Exception {
+        TestThat.theInput("{@include [lines=-1..-2] res:import/lineNumbers.txt}").results("19\n18\n");
+    }
+
+    @Test
+    @DisplayName("A one-line range can be specified with a single number")
+    void testOneLineRange() throws Exception {
+        TestThat.theInput("{@include [lines=1] res:import/lineNumbers.txt}").results("01\n");
+    }
+
+    @Test
+    @DisplayName("A one-line range can be specified with a single number along with multiple ranges")
+    void testOneLineRangeWithMultipleRanges() throws Exception {
+        TestThat.theInput("{@include [lines=1..2,7,-2..-1] res:import/lineNumbers.txt}").results("01\n02\n07\n18\n19\n");
+    }
+
+    @Test
+    @DisplayName("A range can also descent")
+    void testDescendingRange() throws Exception {
+        TestThat.theInput("{@include [lines=10..1] res:import/lineNumbers.txt}").results("10\n09\n08\n07\n06\n05\n04\n03\n02\n01\n");
+    }
+
+    @Test
+    @DisplayName("Ranges can be specified with spaces arbitrary in the middle")
+    void testRangeWithSpaces() throws Exception {
+        TestThat.theInput("{@include [lines=\"1.. 2,  3.. 4,  5.. 7\"] res:import/lineNumbers.txt}").results("01\n02\n03\n04\n05\n06\n07\n");
+    }
+
+    @Test
+    @DisplayName("Range cannot contain zero as a boundary value")
+    void testRangeWithZero() throws Exception {
+        TestThat.theInput("{@include [lines=0..1] res:import/lineNumbers.txt}").throwsBadSyntax("The line range 0\\.\\.1 is not valid");
+    }
+
+    @Test
+    @DisplayName("Range boundary too large just gets capped")
+    void testRangeWithTooLargeBoundary() throws Exception {
+        TestThat.theInput("{@include [lines=18..100] res:import/lineNumbers.txt}").results("18\n19\n");
+    }
+
+    @Test
+    @DisplayName("Range boundary too small just gets capped")
+    void testRangeWithTooSmallBoundary() throws Exception {
+        TestThat.theInput("{@include [lines=-100..+1] res:import/lineNumbers.txt}").results("01\n");
+    }
+
+}
