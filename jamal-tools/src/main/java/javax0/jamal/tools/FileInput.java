123c634cf788201785841e0e19d22611e51da665
Peter Verhas <peter@verhas.com>
2022-02-23 14:47:50 +0100

    FileInput converts \r\n to \n when it reads text on Windows
    Signed-off-by: Peter Verhas <peter@verhas.com>

--- jamal-tools/src/main/java/javax0/jamal/tools/FileInput.java
+++ jamal-tools/src/main/java/javax0/jamal/tools/FileInput.java
@@ -1,6 +1,7 @@
 package javax0.jamal.tools;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -14,6 +15,11 @@ public class FileInput {
      * @throws IOException if the file cannot be read
      */
     static String getInput(String fileName) throws IOException {
-        return Files.readString(Paths.get(fileName));
+        return Files.readString(Paths.get(fileName), StandardCharsets.UTF_8)
+                .chars()
+                .filter(c -> c != '\r')
+                .mapToObj(c -> (char) c)
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
     }
 }
