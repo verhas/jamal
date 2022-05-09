c4a2d288bcefff2a67b256e059b361ce207cd6aa
Peter Verhas <peter@verhas.com>
2022-05-09 17:45:52 +0200

    1.12.3-SNAPSHOT
    Signed-off-by: Peter Verhas <peter@verhas.com>

--- jamal.sh
+++ jamal.sh
@@ -2,7 +2,7 @@
 MODULES="api engine core tools cmd word snippet scriptbasic groovy ruby plantuml debug jamal yaml io assertions jamal"
 REPO=$HOME/.m2/repository
 REPO_JAMAL=$REPO/com/javax0/jamal
-VERSION=1.12.2
+VERSION=1.12.3-SNAPSHOT
 CENTRAL=https://repo1.maven.org/maven2
 
 for MODULE in $MODULES ; do
