c864df897b4001a0bf44656a9e9016db32513229
Mihaly Verhas <misi.verhas@gmail.com>
2022-06-20 20:41:49 +0200

    Convert indents

--- jamal-snippet/src/main/java/javax0/jamal/snippet/SortLines.java
+++ jamal-snippet/src/main/java/javax0/jamal/snippet/SortLines.java
@@ -11,17 +11,17 @@ import static javax0.jamal.tools.InputHandler.skipWhiteSpaces;
 
 public class SortLines implements Macro {
 
-	@Override
-	public String evaluate(Input in, Processor processor) throws BadSyntax {
-		skipWhiteSpaces(in);
-		boolean endsWithNewline = in.toString().endsWith("\n");
-		String[] lines = in.toString().split("\n");
-		Arrays.sort(lines);
-		return String.join("\n", lines) + (endsWithNewline ? "\n" : "");
-	}
+    @Override
+    public String evaluate(Input in, Processor processor) throws BadSyntax {
+        skipWhiteSpaces(in);
+        boolean endsWithNewline = in.toString().endsWith("\n");
+        String[] lines = in.toString().split("\n");
+        Arrays.sort(lines);
+        return String.join("\n", lines) + (endsWithNewline ? "\n" : "");
+    }
 
-	@Override
-	public String getId() {
-		return "sortLines";
-	}
+    @Override
+    public String getId() {
+        return "sortLines";
+    }
 }
--- jamal-snippet/src/test/java/javax0/jamal/snippet/TestSortLines.java
+++ jamal-snippet/src/test/java/javax0/jamal/snippet/TestSortLines.java
@@ -6,33 +6,33 @@ import org.junit.jupiter.api.Test;
 
 public class TestSortLines {
 
-	@Test
-	@DisplayName("sortLines - simple sorting")
-	void testSortLines() throws Exception {
-		TestThat.theInput(""
-				+ "{@sortLines b\n"
-				+ "a\n"
-				+ "d\n"
-				+ "c}"
-		).results("a\n"
-				+ "b\n"
-				+ "c\n"
-				+ "d");
-	}
+    @Test
+    @DisplayName("sortLines - simple sorting")
+    void testSortLines() throws Exception {
+        TestThat.theInput(""
+                + "{@sortLines b\n"
+                + "a\n"
+                + "d\n"
+                + "c}"
+        ).results("a\n"
+                + "b\n"
+                + "c\n"
+                + "d");
+    }
 
-	@Test
-	@DisplayName("sortLines - preserves newline at the end")
-	void testSortLinesPreservesNewline() throws Exception {
-		TestThat.theInput(""
-				+ "{@sortLines b\n"
-				+ "a\n"
-				+ "d\n"
-				+ "c\n"
-				+ "}"
-		).results("a\n"
-				+ "b\n"
-				+ "c\n"
-				+ "d\n");
-	}
+    @Test
+    @DisplayName("sortLines - preserves newline at the end")
+    void testSortLinesPreservesNewline() throws Exception {
+        TestThat.theInput(""
+                + "{@sortLines b\n"
+                + "a\n"
+                + "d\n"
+                + "c\n"
+                + "}"
+        ).results("a\n"
+                + "b\n"
+                + "c\n"
+                + "d\n");
+    }
 
 }
