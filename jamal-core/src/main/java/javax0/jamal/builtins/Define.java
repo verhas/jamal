e70658ce9b49701bd3776b6484185330f73b4d55
Peter Verhas <peter@verhas.com>
2022-07-02 18:14:13 +0200

    define has options for all the special define versions, like global, pure, etc...

--- jamal-core/src/main/java/javax0/jamal/builtins/Define.java
+++ jamal-core/src/main/java/javax0/jamal/builtins/Define.java
@@ -6,6 +6,7 @@ import javax0.jamal.api.Identified;
 import javax0.jamal.api.Input;
 import javax0.jamal.api.Macro;
 import javax0.jamal.api.Processor;
+import javax0.jamal.tools.Params;
 
 import static javax0.jamal.api.SpecialCharacters.DEFINE_OPTIONALLY;
 import static javax0.jamal.api.SpecialCharacters.DEFINE_VERBATIM;
@@ -21,19 +22,36 @@ import static javax0.jamal.tools.InputHandler.skipWhiteSpaces;
 public class Define implements Macro {
     @Override
     public String evaluate(Input input, Processor processor) throws BadSyntax {
+        final var verbatimParam = Params.<Boolean>holder(null, "verbatim").asBoolean();
+        final var optionalParam = Params.<Boolean>holder(null, "optional", "ifNotDefined").asBoolean();
+        final var noRedefineParam = Params.<Boolean>holder(null, "fail", "noRedefine", "noRedef", "failIfDefined").asBoolean();
+        final var pureParam = Params.<Boolean>holder(null, "pure").asBoolean();
+        final var globalParam = Params.<Boolean>holder(null, "global").asBoolean();
+        Params.using(processor).from(this).between("[]").keys(verbatimParam, optionalParam, noRedefineParam, pureParam, globalParam).parse(input);
+        if (noRedefineParam.is() && optionalParam.is()) {
+            throw new BadSyntax(String.format("You cannot use %s and %s", optionalParam.name(), noRedefineParam.name()));
+        }
         skipWhiteSpaces(input);
-        boolean verbatim = false;
-        if (firstCharIs(input, DEFINE_VERBATIM)) {
+        boolean verbatim = verbatimParam.is();
+        if (!verbatim && firstCharIs(input, DEFINE_VERBATIM)) {
             verbatim = true;
             skip(input, 1);
             skipWhiteSpaces(input);
         }
         var optional = firstCharIs(input, DEFINE_OPTIONALLY);
+        if (optionalParam.is() && optional) {
+            throw new BadSyntax(String.format("You cannot use %s and '?' in the define at the same time.", optionalParam.name()));
+        }
         var noRedefine = firstCharIs(input, ERROR_REDEFINE);
+        if (noRedefineParam.is() && noRedefine) {
+            throw new BadSyntax(String.format("You cannot use %s and '!' in the define at the same time.", noRedefineParam.name()));
+        }
         if (optional || noRedefine) {
             skip(input, 1);
             skipWhiteSpaces(input);
         }
+        optional = optional || optionalParam.is();
+        noRedefine = noRedefine || noRedefineParam.is();
         if (!verbatim && firstCharIs(input, DEFINE_VERBATIM)) {
             verbatim = true;
             skip(input, 1);
@@ -63,12 +81,12 @@ public class Define implements Macro {
         }
         skip(input, 1);
         final var macro = processor.newUserDefinedMacro(convertGlobal(id), input.toString(), verbatim, params);
-        if (isGlobalMacro(id)) {
+        if (globalParam.is() || isGlobalMacro(id)) {
             processor.defineGlobal(macro);
         } else {
             processor.define(macro);
         }
-        if (pure && macro instanceof Configurable) {
+        if ((pure || pureParam.is()) && macro instanceof Configurable) {
             ((Configurable) macro).configure("pure", true);
         }
         return "";
--- jamal-test/src/test/java/javax0/jamal/test/core/TestErrorPositions.java
+++ jamal-test/src/test/java/javax0/jamal/test/core/TestErrorPositions.java
@@ -8,32 +8,25 @@ import org.junit.jupiter.api.Test;
 public class TestErrorPositions {
 
     @Test
-    void testSimpleErrors()throws Exception{
-        try {
-            TestThat.theInput("{@options failfast}{@define gulp={zumi}}{gulp {@define [verbatem] zumi=imuz}}").results("");
-        }catch (final BadSyntax bs){
-            Assertions.assertEquals("define '[verbatem]' has no '=' to body at null/1:55",bs.getMessage());
-        }
+    void testSimpleErrors() throws Exception {
+        TestThat.theInput("{@options failfast}{@define gulp={zumi}}{gulp {@define verbatim zumi=imuz}}").throwsBadSyntax("define 'verbatim' has no '=' to body.*");
     }
 
     @Test
-    void errorInMacroParameter()throws Exception{
+    void errorInMacroParameter() throws Exception {
         try {
             //                          1111111111222222222233333333334444444444555555555566666666667777
             //                 1234567890123456789012345678901234567890123456789012345678901234567890123
             TestThat.theInput("{@options failfast}{@define gulp(1,2,3)={zumi123}}{gulp /one/tw{o}/three}").results("");
-        }catch (final BadSyntax bs){
-            Assertions.assertEquals("User defined macro '{o ...' is not defined. Did you mean '@for', '@if'? at null/1:61 <<< null/1:58",bs.getMessage());
+        } catch (final BadSyntax bs) {
+            Assertions.assertEquals("User defined macro '{o ...' is not defined. Did you mean '@for', '@if'? at null/1:61 <<< null/1:58", bs.getMessage());
         }
     }
+
     @Test
-    void errorInPostMacroEvaluation()throws Exception{
-        try {
-            //                          11111111112222222222333333333344444444445555555555666666666677777777778888888888999999999900000
-            //                 12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234
-            TestThat.theInput("{@options failfast}{@define gulp(1,2,3)={zumi123}}{@define zumione=kakk}{gulp /one/{@ident {two}}/three}").results("");
-        }catch (final BadSyntax bs){
-            Assertions.assertEquals("User defined macro '{two ...' is not defined. Did you mean '@try'? at null/1:89 <<< null/1:80",bs.getMessage());
-        }
+    void errorInPostMacroEvaluation() throws Exception {
+        //                          11111111112222222222333333333344444444445555555555666666666677777777778888888888999999999900000
+        //                 12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234
+        TestThat.theInput("{@options failfast}{@define gulp(1,2,3)={zumi123}}{@define zumione=kakk}{gulp /one/{@ident {two}}/three}").throwsBadSyntax("User defined macro '\\{two ...' is not defined. Did you mean '@try'.*");
     }
 }
