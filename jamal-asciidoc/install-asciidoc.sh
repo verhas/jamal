83dd3625c44ba8f34ddbfed18bb91d2bbe9fa3ce
Peter Verhas <peter@verhas.com>
2022-10-12 17:08:22 +0200

    version 1.12.4-SNAPSHOT

--- jamal-asciidoc/install-asciidoc.sh
+++ jamal-asciidoc/install-asciidoc.sh
@@ -6,5 +6,5 @@ mvn clean install
 mkdir -p ../.asciidoctor/lib
 pushd ../.asciidoctor/lib
 rm -f *
-unzip ~/.m2/repository/com/javax0/jamal/jamal-asciidoc/1.12.3/jamal-asciidoc-1.12.3-jamal-asciidoc-distribution.zip >/dev/null
+unzip ~/.m2/repository/com/javax0/jamal/jamal-asciidoc/1.12.4-SNAPSHOT/jamal-asciidoc-1.12.4-SNAPSHOT-jamal-asciidoc-distribution.zip >/dev/null
 popd
