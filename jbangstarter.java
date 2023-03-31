e7c6010ef02b372f00b9d596d9007175a3f3a772
Peter Verhas <peter@verhas.com>
2023-03-31 16:18:11 +0200

    2.0.0 release

--- jbangstarter.java
+++ jbangstarter.java
@@ -1,27 +1,23 @@
 ///usr/bin/env jbang \"$0\" \"$@\" ; exit $? jaja
 //JAVA 11+
-//DEPS com.javax0.jamal:jamal-assertions:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-groovy:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-io:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-jamal:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-markdown:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-mock:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-plantuml:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-ruby:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-scriptbasic:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-snippet:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-yaml:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-word:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-prog:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-maven-load:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-engine:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-api:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-tools:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-core:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-cmd:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-debug:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-maven-input:1.12.7-SNAPSHOT
-//DEPS com.javax0.jamal:jamal-jar-input:1.12.7-SNAPSHOT
+//DEPS com.javax0.jamal:jamal-assertions:2.0.0
+//DEPS com.javax0.jamal:jamal-jamal:2.0.0
+//DEPS com.javax0.jamal:jamal-markdown:2.0.0
+//DEPS com.javax0.jamal:jamal-mock:2.0.0
+//DEPS com.javax0.jamal:jamal-plantuml:2.0.0
+//DEPS com.javax0.jamal:jamal-snippet:2.0.0
+//DEPS com.javax0.jamal:jamal-yaml:2.0.0
+//DEPS com.javax0.jamal:jamal-json:2.0.0
+//DEPS com.javax0.jamal:jamal-prog:2.0.0
+//DEPS com.javax0.jamal:jamal-maven-load:2.0.0
+//DEPS com.javax0.jamal:jamal-engine:2.0.0
+//DEPS com.javax0.jamal:jamal-api:2.0.0
+//DEPS com.javax0.jamal:jamal-tools:2.0.0
+//DEPS com.javax0.jamal:jamal-core:2.0.0
+//DEPS com.javax0.jamal:jamal-cmd:2.0.0
+//DEPS com.javax0.jamal:jamal-debug:2.0.0
+//DEPS com.javax0.jamal:jamal-maven-input:2.0.0
+//DEPS com.javax0.jamal:jamal-jar-input:2.0.0
 
 
 
