842616274d57974418af128b13c70f0a575537cc
Peter Verhas <peter@verhas.com>
2023-05-06 01:44:53 +0200

    readme updated

--- jamal-test/TEST_PLAYGORUND_MNI_O1G.java
+++ jamal-test/TEST_PLAYGORUND_MNI_O1G.java
@@ -3,12 +3,9 @@ public class TEST_PLAYGORUND_MNI_O1G {
 
     //<editor-fold defaultstate="collapsed" desc="This code is generated">
 
-        final static private String a = "a";
-        final static private String b = "b";
-        final static private String c = "c";
-        final static private String d = "d";
-        final static private String e = "e";
-        final static private String f = "f";
+
+dddd This is just Java Code
+
 
     //</editor-fold>
 
