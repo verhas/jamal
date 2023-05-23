679c252e69b1fea6e32c7feaf3a04ed2a910fb44
Peter Verhas <peter@verhas.com>
2023-05-23 17:03:54 +0200

    relase 2.1.0

--- jbangstarter.java
+++ jbangstarter.java
@@ -1,23 +1,23 @@
 ///usr/bin/env jbang \"$0\" \"$@\" ; exit $? jaja
 //JAVA 11+
-//DEPS com.javax0.jamal:jamal-assertions:2.0.2
-//DEPS com.javax0.jamal:jamal-jamal:2.0.2
-//DEPS com.javax0.jamal:jamal-markdown:2.0.2
-//DEPS com.javax0.jamal:jamal-mock:2.0.2
-//DEPS com.javax0.jamal:jamal-plantuml:2.0.2
-//DEPS com.javax0.jamal:jamal-snippet:2.0.2
-//DEPS com.javax0.jamal:jamal-yaml:2.0.2
-//DEPS com.javax0.jamal:jamal-json:2.0.2
-//DEPS com.javax0.jamal:jamal-prog:2.0.2
-//DEPS com.javax0.jamal:jamal-maven-load:2.0.2
-//DEPS com.javax0.jamal:jamal-engine:2.0.2
-//DEPS com.javax0.jamal:jamal-api:2.0.2
-//DEPS com.javax0.jamal:jamal-tools:2.0.2
-//DEPS com.javax0.jamal:jamal-core:2.0.2
-//DEPS com.javax0.jamal:jamal-cmd:2.0.2
-//DEPS com.javax0.jamal:jamal-debug:2.0.2
-//DEPS com.javax0.jamal:jamal-maven-input:2.0.2
-//DEPS com.javax0.jamal:jamal-jar-input:2.0.2
+//DEPS com.javax0.jamal:jamal-assertions:2.1.0
+//DEPS com.javax0.jamal:jamal-jamal:2.1.0
+//DEPS com.javax0.jamal:jamal-markdown:2.1.0
+//DEPS com.javax0.jamal:jamal-mock:2.1.0
+//DEPS com.javax0.jamal:jamal-plantuml:2.1.0
+//DEPS com.javax0.jamal:jamal-snippet:2.1.0
+//DEPS com.javax0.jamal:jamal-yaml:2.1.0
+//DEPS com.javax0.jamal:jamal-json:2.1.0
+//DEPS com.javax0.jamal:jamal-prog:2.1.0
+//DEPS com.javax0.jamal:jamal-maven-load:2.1.0
+//DEPS com.javax0.jamal:jamal-engine:2.1.0
+//DEPS com.javax0.jamal:jamal-api:2.1.0
+//DEPS com.javax0.jamal:jamal-tools:2.1.0
+//DEPS com.javax0.jamal:jamal-core:2.1.0
+//DEPS com.javax0.jamal:jamal-cmd:2.1.0
+//DEPS com.javax0.jamal:jamal-debug:2.1.0
+//DEPS com.javax0.jamal:jamal-maven-input:2.1.0
+//DEPS com.javax0.jamal:jamal-jar-input:2.1.0
 
 
 
