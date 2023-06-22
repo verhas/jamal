1eb2c5c9d400ee068cceb35765310de28aac6adb
Peter Verhas <peter@verhas.com>
2023-06-22 13:12:27 +0200

    documentation updated

--- jamal-prog/src/main/java/javax0/jamal/prog/commands/FunctionCall.java
+++ jamal-prog/src/main/java/javax0/jamal/prog/commands/FunctionCall.java
@@ -21,29 +21,29 @@ public class FunctionCall extends Expression {
             arguments[i] = this.arguments[i].execute(context);
         }
 
-        final var macro = context.getProcessor().getRegister().getMacro(function);
-        if (macro.isPresent()) {
-            final var m = macro.get();
-            final String open, close;
-            open = m.optionsStart();
-            close = m.optionsEnd();
-
-            final var argBuilder = new StringBuilder();
-            if (arguments.length > 1) {
-                argBuilder.append(open);
-                for (int i = 0; i < arguments.length - 1; i++) {
-                    argBuilder.append(arguments[i]).append(" ");
-                }
-                argBuilder.append(close);
-            }
-            if (arguments.length > 0) {
-                argBuilder.append(arguments[arguments.length - 1]);
-            }
-            return macro.get().evaluate(Input.makeInput(argBuilder.toString()), context.getProcessor());
+        final var ud = context.getProcessor().getRegister().getUserDefined(function);
+        if (ud.isPresent() && ud.get() instanceof Evaluable) {
+            return ((Evaluable) ud.get()).evaluate(arguments);
         } else {
-            final var ud = context.getProcessor().getRegister().getUserDefined(function);
-            if (ud.isPresent() && ud.get() instanceof Evaluable) {
-                return ((Evaluable) ud.get()).evaluate(arguments);
+            final var macro = context.getProcessor().getRegister().getMacro(function);
+            if (macro.isPresent()) {
+                final var m = macro.get();
+                final String open, close;
+                open = m.optionsStart();
+                close = m.optionsEnd();
+
+                final var argBuilder = new StringBuilder();
+                if (arguments.length > 1) {
+                    argBuilder.append(open);
+                    for (int i = 0; i < arguments.length - 1; i++) {
+                        argBuilder.append(arguments[i]).append(" ");
+                    }
+                    argBuilder.append(close);
+                }
+                if (arguments.length > 0) {
+                    argBuilder.append(arguments[arguments.length - 1]);
+                }
+                return macro.get().evaluate(Input.makeInput(argBuilder.toString()), context.getProcessor());
             } else {
                 throw new BadSyntax(String.format("Macro for the function '%s()' is not defined.", function));
             }
