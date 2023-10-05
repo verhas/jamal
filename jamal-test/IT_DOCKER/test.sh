f252feb801ccff71d94c5061ea48d159aa17275f
Peter Verhas <peter@verhas.com>
2023-10-05 12:43:28 +0200

    Integration test script checks that it is in docker environment and stops if it is accidentally started from the dev env. versions updated, finally we could update JavaDoc to 3.6.0 skipping the faulty 3.5.0 integration test MD converted to ASCIIDOC

--- jamal-test/IT_DOCKER/test.sh
+++ jamal-test/IT_DOCKER/test.sh
@@ -2,6 +2,6 @@
 chmod 0700 ../../jamal.sh
 ../../jamal.sh ../../jamal-maven-load/it.sh.jam ../../jamal-maven-load/it.sh
 ../../jamal.sh integrationtest.jam integrationtest
-../../jamal.sh README.md.jam README.md
+../../jamal.sh README.adoc.jam README.adoc
 docker build -t jamal-test .
 docker run -it jamal-test
