8434275e966dd5218266ab4db804845fe0f5534c
Peter Verhas <peter@verhas.com>
2023-11-22 13:02:22 +0100

    JavaDoc documentation was added and also  some of the parop documentation in the io module was moved to the source code

--- jamal-io/src/main/java/javax0/jamal/io/Copy.java
+++ jamal-io/src/main/java/javax0/jamal/io/Copy.java
@@ -16,12 +16,27 @@ public class Copy implements Macro, Scanner.WholeInput {
     @Override
     public String evaluate(Input in, Processor processor) throws BadSyntax {
         final var scanner = newScanner(in, processor);
+        // snippet copy_options
         final var from = scanner.str(null, "from");
+        // should specify the source file.
+        // It is usually a URL starting with `https://`.
+        // Note that Jamal deliberately does not support `http://` URLs.
         final var to = scanner.str(null, "to");
+        // the target file name.
+        // This is where the file will be saved.
         final var append = scanner.bool("io:append", "append");
+        // is a boolean parameter meaning that the file should be appended.
         final var mkdir = scanner.bool("io:mkdir", "mkdir");
+        // is a boolean parameter meaning that the directory where the file
+        // should be saved should be created if it does not exist.
         final var useCache = scanner.bool("cache");
+        //  is a boolean parameter meaning that the file download cache implemented in Jamal should be used.
         final var overwrite = scanner.bool("overwrite");
+        // is a boolean parameter meaning that the file should be overwritten if it exists.
+        // If this parameter is not specified and the target file exists, then the macro will not even start the copy.
+        // The same effect could be reached surrounding the `io:copy` macro with an `if` macro using {%link file%} checking the existence of the target file.
+        // This option is for convenience.
+        // end snippet
         scanner.done();
 
         final var toName = Utils.getFile(to, in);
--- jamal-io/src/main/java/javax0/jamal/io/Utils.java
+++ jamal-io/src/main/java/javax0/jamal/io/Utils.java
@@ -3,20 +3,43 @@ package javax0.jamal.io;
 import javax0.jamal.api.BadSyntax;
 import javax0.jamal.api.Input;
 import javax0.jamal.tools.FileTools;
-import javax0.jamal.tools.Params;
 import javax0.jamal.tools.Scanner;
 import javax0.jamal.tools.param.BooleanParameter;
 import javax0.jamal.tools.param.StringParameter;
 
-public class Utils {
+/**
+ * A few little utility functions used by different macros.
+ */
+class Utils {
+
+    /**
+     * Create and return a {@link StringParameter} that can be used to get the output file name from the input.
+     *
+     * @param scanner the scanner to use to get the parameter
+     * @return the parameter
+     */
     static StringParameter getFile(final Scanner.ScannerObject scanner) {
         return scanner.str("io:outputFile", "io:output", "output", "io:file", "file");
     }
 
+    /**
+     * Create and return a {@link BooleanParameter} that can be used to get the recursive flag from the input.
+     *
+     * @param scanner the scanner to use to get the parameter
+     * @return the parameter
+     */
     static BooleanParameter getRecursive(final Scanner.ScannerObject scanner) {
         return scanner.bool("io:recursive", "recursive");
     }
 
+    /**
+     * Get the absolute file name from the parameter {@code file} and the reference {@code in}.
+     *
+     * @param file the parameter that contains the file name
+     * @param in the input that is used as a reference
+     * @return the absolute file name
+     * @throws BadSyntax if the file name is not a valid file name
+     */
     static String getFile(StringParameter file, Input in) throws BadSyntax {
         final var reference = in.getReference();
         return FileTools.absolute(reference, file.get());
