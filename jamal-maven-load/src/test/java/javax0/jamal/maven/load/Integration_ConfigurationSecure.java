c0e95453bb9737db3a4dcc741c8fe9716a108208
Peter Verhas <peter@verhas.com>
2025-02-04 13:10:59 +0100

    integration test version maven test

--- jamal-maven-load/src/test/java/javax0/jamal/maven/load/Integration_ConfigurationSecure.java
+++ jamal-maven-load/src/test/java/javax0/jamal/maven/load/Integration_ConfigurationSecure.java
@@ -1,19 +1,21 @@
 package javax0.jamal.maven.load;
 
-import javax0.jamal.api.Processor;
 import javax0.jamal.testsupport.TestThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
  * This integration test should be executed when the environment is configured in a way so that the configuration
- * is not secure.
+ * is secure.
+ * <p>
+ * It uses the version 2.8.2, which was the latest release by the time of writing this file. The actual version does
+ * not matter as long as it is available from the central repo.
  */
 public class Integration_ConfigurationSecure {
-    @DisplayName("Non-secure configuration fails any maven:load")
+    @DisplayName("Secure configuration runs any maven:load")
     @Test
-    void test()throws Exception {
-        TestThat.theInput(String.format( "{@maven:load com.javax0.jamal:jamal-test:%s}", Processor.jamalVersionString()))
+    void test() throws Exception {
+        TestThat.theInput("{@maven:load com.javax0.jamal:jamal-test:2.8.2}")
                 .results("");
     }
 
